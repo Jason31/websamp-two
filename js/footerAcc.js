c6c7602ac23ae353c2d19792520756571f0ea153
Jason <51981416+Jason31@users.noreply.github.com>
2022-05-24 23:06:17 -0700

    Add files via upload

---
+++ js/footerAcc.js
@@ -0,0 +1,5 @@
+  jQuery(".nav-folderized h5").click(function(){
+	  jQuery(this).parent(".nav").toggleClass("open"); 
+	  jQuery('html, body').animate({ scrollTop: jQuery(this).offset().top - 170 }, 1500 );
+  });
+  
