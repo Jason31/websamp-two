75be501ab5d0675cddb219743cc97ca9d15f83a7
Jason <51981416+Jason31@users.noreply.github.com>
2022-06-21 11:42:48 +0800

    Update footerAcc.js

--- js/footerAcc.js
+++ js/footerAcc.js
@@ -1,4 +1,4 @@
-  jQuery(".nav-folderized h5").click(function(){
+  jQuery(".nav-folderized h3").click(function(){
 	  jQuery(this).parent(".nav").toggleClass("open"); 
 	  jQuery('html, body').animate({ scrollTop: jQuery(this).offset().top - 170 }, 1500 );
   });
